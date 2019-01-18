25abc6a5ffef5c9109f1ca52a0fcd844c9dd3c1d
kiefer <eero.manty@gmail.com>
2019-01-18 14:24:28 +0200

    add timer

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,30 @@ pub use self::camera::CameraBuilder;
 pub use self::io::read_obj_file;
 pub use self::world::entities::TestBall;
 
+use std::time::Instant;
+
+pub struct Timer {
+    moments: Vec<Instant>
+}
+
+impl Timer{
+    pub fn new() -> Timer{
+        Timer{moments: Vec::new()}
+    }
+
+
+    pub fn add(&mut self){
+
+        self.moments.push(Instant::now());
+    }
+    pub fn print(&self){
+        for i in 1 .. self.moments.len() {
+            let dur = self.moments[i].duration_since(self.moments[i-1]);
+            print!("{}:{} ", dur.as_secs(),dur.subsec_millis());
+        }
+    }
+}
+
 
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-
-
 use ray_tracer as rt;
 
-
 fn main() {
+    let mut timer = rt::Timer::new();
+    timer.add();
     let t =rt::read_obj_file("cornell.obj").unwrap();
-    println!("file read.");
+    timer.add();
     let wb = rt::WorldBuilder::from_entities(t);
     /*let mut wb = rt::WorldBuilder::new();
     let ball = rt::TestBall{c:rt::Color{r:1.0, g:0.0,b:0.0},r:1.2, o: rt::Vector::new(0.5, 0.0, 0.0)};
@@ -13,14 +12,16 @@ fn main() {
     wb.add_entity(ball);
     wb.add_entity(ball2);*/
     let world = wb.build();
-    println!("World created");
+    timer.add();
     let mut cam_conf = rt::CameraBuilder::new();
     cam_conf.orig = rt::Vector::new(0.0,0.0,3.0);
     cam_conf.look_at(rt::Vector::new(0.0,0.0,0.0));
     let cam =  cam_conf.build();
-    println!("Camera created.");
+    timer.add();
     let p = cam.take_pic(&world);
-    println!("Picture taken.");
+    timer.add();
     p.save("test.png").unwrap();
+    timer.add();
     println!("Finish");
+    timer.print();
 }
